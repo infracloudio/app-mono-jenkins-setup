b7d73dce0256b1ca9082ca6173f429b6324e9f56
Prasad Ghangal <prasad.ghangal@gmail.com>
2018-12-02 19:26:09 +0530

    Add job dsl script to create jenkins jobs

---
+++ job_dsl.groovy
@@ -0,0 +1,51 @@
+pipelineJob('k8s-deploy') {
+
+  def repo = 'https://github.com/infracloudio/app-mono-helmstate.git'
+
+  triggers {
+    githubPush()
+  }
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      lightweight(true)
+      scm {
+        git {
+          remote {
+            credentials('github-credentials')
+            url(repo)
+          }
+          branch('master')
+          scriptPath('Jenkinsfile')
+          extensions { }
+        }
+      }
+    }
+  }
+}
+
+
+pipelineJob('image-orchestrator-job') {
+
+  def repo = 'https://github.com/infracloudio/app-mono-orchestrator.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      lightweight(true)
+      scm {
+        git {
+          remote {
+            credentials('github-credentials')
+            url(repo)
+          }
+          branch('master')
+          scriptPath('Jenkinsfile')
+          extensions { }
+        }
+      }
+    }
+  }
+}
